0040d8c6bab8f5b59ef6eceb9053e569941a83a4
matheumss <matheusmanoelmmss@gmail.com>
2026-09-03 14:24:54 -0300

    cookie an sripts package

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,6 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import CookieParser from 'cookie-parser';
 import helmet from 'helmet';
 
 import { AppModule } from './app.module';
@@ -11,7 +13,18 @@ async function bootstrap() {
   app.useGlobalFilters(new HttpExceptionFilter());
   app.enableCors();
   app.useGlobalPipes();
+  app.use(CookieParser());
   app.use(helmet());
+
+  const config = new DocumentBuilder()
+    .setTitle('Starter Nest API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
